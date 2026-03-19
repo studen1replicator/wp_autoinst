b42402fef683b278751780f3538f0fc972219d33
student_replicator <somwhere@in.space>
2026-03-19 19:59:08 +0300

    version 0.9

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+17.03
 Still in progress
 php version 8.1 for ubuntu 22.04
+
+19.03
+Changed script: repaired an errors. Need to test on clear machine
+TO DO:
+  write database name, user and password to wordpress config (see last cp command)
--- wp_stack.sh
+++ wp_stack.sh
@@ -2,12 +2,18 @@
 
 # ___INSTALL___
 
+
 # Prepare repository:
-curl -o /etc/apt/trusted.gpg.d/angie-signing.gpg \ 
+
+apt install ca-certificates curl -y
+
+curl -o /etc/apt/trusted.gpg.d/angie-signing.gpg \
       https://angie.software/keys/angie-signing.gpg
 
 echo "deb https://download.angie.software/angie/$(. /etc/os-release && echo "$ID/$VERSION_ID $VERSION_CODENAME") main" \
     | sudo tee /etc/apt/sources.list.d/angie.list > /dev/null
+
+apt update
 
 # install angie:
 apt install angie -y
@@ -17,9 +23,9 @@ apt install php8.1-fpm php8.1 php8.1-mysql mysql-server-8.0 php-curl php-gd php-
 
 # wordpress:
 wget https://wordpress.org/latest.tar.gz -P /tmp
-tar xvf /tmp/latest.tar.gz tmp/wordpress/
-mkdir -p /var/www/html
-cp -r /tmp/wordpress/ /var/www/html/
+tar xvfz /tmp/latest.tar.gz
+mkdir -p /var/www/html/
+cp -r wordpress/ /var/www/html/
 mkdir /var/www/html/wordpress/wp-content/uploads
 chmod -R 755 /var/www/html/wordpress/
 chown -R www-data:www-data /var/www/html/wordpress/
@@ -32,4 +38,9 @@ echo "FLUSH PRIVILEGES;" | mysql
 
 cp angie.conf /etc/angie/
 cp default.conf /etc/angie/http.d/
+
+cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+
+systemctl restart angie
+
 
